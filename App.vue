5ca4510fa01921d73f5cf3d13973349f12c7dc17
terehinAV <terehin_murom@mail.ru>
2023-05-25 10:10:24 +0300

    .

--- App.vue
+++ App.vue
@@ -1,15 +1,53 @@
 <template>
   <div id="main">
-    <div v-if="!is_telegram_client" class="text-center">
+    <div
+      v-if="is_telegram_client && is_telegram_api_updated"
+      class="text-center"
+    >
+      <!--<h3>Window Control</h3>
+      <b>isExpanded</b>: {{ TWA.isExpanded }}
+      <button @click="TWA.expand()">Expand</button>
+      <button @click="TWA.close()">Close</button><br>
+      <h3>Functions and buttons</h3>
+      -->
+
+      <div v-if="code">
+        <h3>QR code:</h3>
+        {{ code }} <br>
+
+        <v-btn
+          v-if="is_url"
+          size="large"
+          @click="openLink()"
+        >
+          Open Link
+        </v-btn>
+        <!--<button @click="copyCodeClipboard()">copy to clipboard</button>-->
+      </div>
+      <div v-if="!code">
+        <h3>Scan a QR code!</h3>
+      </div>
+    </div>
+
+
+
+    <div
+      v-if="!is_telegram_client"
+      class="text-center"
+    >
       Please open the app from a Telegram client!<br>
     </div>
-    <div v-if="is_telegram_client && !is_telegram_api_updated" class="text-center">
+    <div
+      v-if="is_telegram_client && !is_telegram_api_updated"
+      class="text-center"
+    >
       Please update Telegram to Use the app!<br>
       Telegram API version needed 6.4 or greater.<br>
       Your Telegram API version: {{ TWA.version }}
     </div>
   </div>
 </template>
+
 
 <script>
 import { prepareUrl } from './helpers'
@@ -26,8 +64,8 @@ export default {
   },
   created() {
     // Binding function to all the event types
-    // this.TWA.onEvent('themeChanged', this.themeChanged);
-    this.TWA.MainButton.setText("Scan QR");     // LOCALE
+    //this.TWA.onEvent('themeChanged', this.themeChanged);
+    this.TWA.MainButton.setText("Scan QR code");
     this.TWA.onEvent('qrTextReceived', this.processQRCode);
     this.TWA.onEvent('mainButtonClicked', this.mainButtonClicked);
 
@@ -48,11 +86,14 @@ export default {
   },
   methods: {
     // attached with onEvent function during created
-    // themeChanged() {
-    //   this.TWA.showAlert('Theme has changed');
-    // },
+    themeChanged() {
+      //this.TWA.showAlert('Theme has changed');
+    },
     mainButtonClicked() {
-       this.showQRScanner();
+      this.showQRScanner();
+    },
+    openLink() {
+      this.TWA.openLink(this.url);
     },
     processQRCode(data) {
        this.code = data.data;
@@ -61,10 +102,8 @@ export default {
        this.url = result.value;
        this.hapticImpact();
        this.TWA.closeScanQrPopup();
-       navigator.clipboard.writeText(result.value).then(
-            () => {this.TWA.showAlert('Text copied to clipboard', () => {this.TWA.close()});},
-            () => {this.TWA.close()}
-       );
+
+       //this.TWA.showAlert(data.data);
     },
     // End of callbacks
     showQRScanner() {
@@ -76,12 +115,24 @@ export default {
     hapticImpact() {
       // light medium heavy rigid soft
       this.TWA.HapticFeedback.impactOccurred("heavy");
-    }
+    },
+    //copyCodeClipboard() {
+    //  var Url = this.$refs.mylink;
+    //  Url.innerHTML = window.location.href;
+    //  console.log(Url.innerHTML)
+    //  Url.select();
+    //  document.execCommand("copy");
+    //}
   }
 }
 </script>
 
 <style scoped>
+/*
+bg_color            .
+secondary_bg_color  var(--tg-theme-secondary-bg-color)
+link_color          var(--tg-theme-link-color).
+*/
 #main {
   background-color: var(--tg-theme-bg-color, white);
   color: var(--tg-theme-text-color, black);
